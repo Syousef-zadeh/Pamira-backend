6f70dfbd266bb9b76d2a2c6d90d34884abdef5e0
Sheida <syousef-zadeh@myseneca.ca>
2021-09-03 15:57:20 -0400

    delete sign up

--- utils/generateToken.js
+++ utils/generateToken.js
@@ -2,7 +2,7 @@ const jwt = require("jsonwebtoken");
 require("dotenv").config({ path: "../config/keys.env" });
 
 const generateToken = (id) => {
-  return jwt.sign({ id }, `${process.env.JWT_SECRET}`, {
+  return jwt.sign({ id }, "" + process.env.JWT_SECRET, {
     expiresIn: "30d",
   });
 };
