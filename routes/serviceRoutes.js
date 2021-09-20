549527726d2ea7cd8271ed769b6571dedf39541d
Sheida <syousef-zadeh@myseneca.ca>
2021-09-20 10:14:04 -0400

    add edit & delete btn

--- routes/serviceRoutes.js
+++ routes/serviceRoutes.js
@@ -10,7 +10,7 @@ const router = express.Router();
 
 router.route("/").get(services);
 router.route("/add").post(serviceAdd);
-router.route("update:id").put(serviceUpdate);
-router.route("delete/:id").delete(serviceDelete);
+router.route("update/:id").put(serviceUpdate);
+router.route("/:id").delete(serviceDelete);
 
 module.exports = router;
